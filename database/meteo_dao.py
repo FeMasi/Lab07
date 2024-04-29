d6e089e80ca02e7dba4efab60080320a5720b39c
FeMasi <federico.masiello97@gmail.com>
2024-04-29 10:09:36 +0200

    correzione parziale lab 07 (vedere quella del professore perché contiene errori)

--- database/meteo_dao.py
+++ database/meteo_dao.py
@@ -1,11 +1,9 @@
 from database.DB_connect import DBConnect
 from model.situazione import Situazione
 
-
 class MeteoDao():
 
-    @staticmethod
-    def get_all_situazioni():
+    def get_all_sitauzioni(mese):
         cnx = DBConnect.get_connection()
         result = []
         if cnx is None:
@@ -13,13 +11,50 @@ class MeteoDao():
         else:
             cursor = cnx.cursor(dictionary=True)
             query = """SELECT s.Localita, s.Data, s.Umidita
-                        FROM situazione s 
-                        ORDER BY s.Data ASC"""
-            cursor.execute(query)
+                        FROM Situazione s 
+                        GROUP BY s.Data ASC"""
+            cursor.execute(query, (mese,))
             for row in cursor:
-                result.append(Situazione(row["Localita"],
-                                         row["Data"],
-                                         row["Umidita"]))
+                result.append((row["Localita"], row["media"]))
+            cursor.close()
+            cnx.close()
+        return result
+
+    @staticmethod
+    def get_umidita_media_mese(mese):
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("Connessione fallita")
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            query = """SELECT s.Localita, AVG(s.Umidita)
+                        FROM Situazione s 
+                        WHERE MONTH(s.'Data')=&s
+                        GROUP BY s.Localita"""
+            cursor.execute(query, (mese,))
+            for row in cursor:
+                result.append((row["Localita"], row["media"]))
+            cursor.close()
+            cnx.close()
+        return result
+
+
+    def get_situazione_meta_mese(mese):
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("Connessione fallita")
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            query = """SELECT s.Localita, s.Data, s.Umidita
+                        FROM Situazione s 
+                        WHERE MONTH(s.'Data')=&s
+                        AND DAY(s.Data)<=15
+                        GROUP BY s.Data ASC"""
+            cursor.execute(query, (mese,))
+            for row in cursor:
+                result.append((row["Localita"], row["media"]))
             cursor.close()
             cnx.close()
         return result
